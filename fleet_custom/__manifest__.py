152ffe9e3ae513ade2e66781f40215beb17d7ade
planet-odoo <developers@planet-odoo.com>
2019-05-29 18:16:39 +0530

    completed inspection forms and digital signature in fleet

--- fleet_custom/__manifest__.py
+++ fleet_custom/__manifest__.py
@@ -8,7 +8,9 @@
     "website": "http://www.planet-odoo.com",
     "depends": ["base", "fleet",],
     "data": [
+        'security/ir.model.access.csv',
         'views/inspection.xml',
+        'views/annual_inspection.xml',
         'views/daily_inspection.xml',
     ],
     "qweb": [],
--- fleet_custom/models/inspection.py
+++ fleet_custom/models/inspection.py
@@ -1,9 +1,28 @@
 from odoo import api,fields,models,_
 
+class Fleet_Vehicles(models.Model):
+    _inherit = 'fleet.vehicle'
+
+    @api.multi
+    def return_action_for_inspection(self):
+        """ This opens the xml view specified in xml_id for the current vehicle """
+        self.ensure_one()
+        xml_id = self.env.context.get('xml_id')
+        if xml_id:
+            res = self.env['ir.actions.act_window'].for_xml_id('fleet_custom', xml_id)
+            res.update(
+                context=dict(self.env.context, default_vehicle_id=self.id, group_by=False),
+                domain=[('vehicle_id', '=', self.id)]
+            )
+            return res
+        return False
 
 class AnnualInspection(models.Model):
     _name = 'annual.inspection'
 
+    vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle', required=True)
+    annual_digital_signature = fields.Binary('Signature')
+    # annual_count = fields.Integer(compute="_compute_count_annual", string="Annual Count")
     owner = fields.Many2one('res.partner','Owner')
     date = fields.Date('Date')
     hour = fields.Char('Hour')
@@ -39,7 +58,9 @@ class AnnualInspection(models.Model):
     nuts = fields.Boolean('Tuercas')
     liquid = fields.Boolean('Líquido "Power Steering"')
     #Windshield(Parabrisas)
+    windshield = fields.Boolean('Parabrisas')
     #Windshield Wipers(Limpiaparabrisas)
+    windshield_wipers = fields.Boolean('Limpiaparabrisas')
     #Gas(Combustible)
     leaks = fields.Boolean('Filtaciones')
     cover_the_tank = fields.Boolean('Tapa el Tanque')
@@ -65,11 +86,12 @@ class AnnualInspection(models.Model):
     chain = fields.Boolean('Cadena')
 
 
-
 class DailyInspection(models.Model):
-
     _name = 'daily.inspection'
 
+    vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle', required=True)
+    daily_digital_signature = fields.Binary('Signature')
+    # daily_count = fields.Integer(compute="_compute_count_daily", string="Daily Count")
     driver = fields.Many2one('res.partner', "Chofer")
     unity = fields.Integer("Unidad")
     clapboard = fields.Char("Tablilla")
@@ -78,16 +100,12 @@ class DailyInspection(models.Model):
     pm_time = fields.Float("Hora PM")
     am_mileage = fields.Float("Millaje AM")
     pm_mileage = fields.Float("Millaje PM")
-
     #Security Team
-
     safety_equipment = fields.Boolean("Equipo de Seguridad (PPE)")
     fire_extinguisher = fields.Boolean("Extintor de Incendios")
     flag = fields.Boolean("Bandera")
     reflectors = fields.Boolean("Reflectores")
-
     #Cabin
-
     safety_belt_and_seats = fields.Boolean("Cinturón de Seguridad y Asientos")
     guide_and_management_system = fields.Boolean("Guía y Sistema de Dirección")
     rear_view_mirrors = fields.Boolean("Espejos Retrovisores")
@@ -101,15 +119,11 @@ class DailyInspection(models.Model):
     oil_pressure_fuel_other = fields.Boolean("Presión de Aceite, Combustible, Otros")
     voltage_indicator = fields.Boolean("Indicador de Voltaje")
     interior_cleaning = fields.Boolean("Limpieza Interior")
-
     #Lights
-
     front_and_rear_lights = fields.Boolean("Luces Delanteras y Traseras")
     clapboard_lights = fields.Boolean("Luces de Tablilla")
     signals_front_and_rear = fields.Boolean("Señales (Delanteras y Traseras)")
-
     #Exterior and Mechanics
-
     foot_and_handbrakes = fields.Boolean("Frenos de Pie y de Mano")
     rubber_bands_and_rings = fields.Boolean("Gomas y Aros")
     water_and_oil_lichens = fields.Boolean("Liqueos de Agua y de Aceite")
@@ -122,7 +136,5 @@ class DailyInspection(models.Model):
     cargo_and_mooring_devices = fields.Boolean("Carga y Dispositivos de Amarre")
     fifth_wheel_or_pintle_hook = fields.Boolean("Quinta Rueda / Pintle Hook")
     exterior_cleaning = fields.Boolean("Limpieza Exterior")
-
     #Others
-
     others = fields.Boolean("Otros")
